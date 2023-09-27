20741d5b87eac362bc573ec97a0e4a40a1763576
Shanyik <tkrisi213@gmail.com>
2023-09-27 11:01:59 +0200

    Initialzed react and asp.net web api

---
+++ LPH-test/LPH-test/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LPH_test.Controllers;
+
+
+[ApiController]
+[Route("[controller]")]
+public class TestController : ControllerBase
+{
+    [HttpGet("test")]
+    public IActionResult Get()
+    {
+        
+        
+        return Ok("{yes}");
+    }
+}
