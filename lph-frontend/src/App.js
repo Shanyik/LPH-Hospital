d61603f6190cff2cfc1322e920afed18864ce4f5
Shanyik <tkrisi213@gmail.com>
2023-09-27 11:46:25 +0200

    Patient model and basic endpoint

--- lph-frontend/src/App.js
+++ lph-frontend/src/App.js
@@ -1,26 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
-  fetch('http://localhost:5227/Test/test')
-    .then(response => console.log(response.status))
+  fetch('http://localhost:5227/Patient/GetAll')
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
